8dddb1178f334cf0fbc43e80ba0de77e8a4509b0
George Hahn <george@genericmaker.com>
2015-10-23 22:13:26 -0400

    Add ability to set/retrieve friendly enum names (-not I8N friendly-)

--- Models/ModelBag.cs
+++ Models/ModelBag.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Nancy.ViewEngines.Razor.HtmlHelpers;
 using ServiceStack.DataAnnotations;
 
 namespace CS431_Project.Models
@@ -11,9 +13,9 @@ namespace CS431_Project.Models
     {
         [AutoIncrement]
         [PrimaryKey]
-        int MovieId { get; set; }
+        public int MovieId { get; set; }
 
-        string Title;
+        public string Title;
 
         public int Duration { get; set; }
 
@@ -21,8 +23,11 @@ namespace CS431_Project.Models
 
         public enum genre
         {
+            [FriendlyName("Commedy")]
             Commedy,
+            [FriendlyName("Action")]
             Action,
+            [FriendlyName("Other")]
             Etc
         }
 
@@ -34,30 +39,35 @@ namespace CS431_Project.Models
         {
             return $"{MovieId}: {Title}, {Duration} minutes, {ReleaseDate} release, {Genre}";
         }
+
+        public string GetPrettyTitleLink()
+        {
+            return Title.ToLower().Replace(' ', '-'); // THIS ISN'T GOOD ENOUGH FOR URL ENCODING BUT WHATEVER. TODO.
+        }
     }
 
     public class Customer
     {
         [AutoIncrement]
         [PrimaryKey]
-        int CustomerId { get; set; }
-        string Name;
+        public int CustomerId { get; set; }
+        public string Name;
     }
 
     public class Showing
     {
         [AutoIncrement]
         [PrimaryKey]
-        int ShowingId { get; set; }
-        decimal Price;
-        
+        public int ShowingId { get; set; }
+        public decimal Price;
+
         // This probably gets serialized as a blob
         // Should be changed to a numeric type if we want to enable eg sorting by duration
-        DateTime Time;
-        int ScreenNumber;
-        int TotalSeats;
-        int AvailableSeats;
-        int MovieId;
+        public DateTime Time;
+        public int ScreenNumber;
+        public int TotalSeats;
+        public int AvailableSeats;
+        public int MovieId;
 
         [Reference]
         public Movie Movie { get; set; }
@@ -67,13 +77,13 @@ namespace CS431_Project.Models
     {
         [AutoIncrement]
         [PrimaryKey]
-        int PurchaseId { get; set; }
-        int MovieId;
-        int ShowingId;
-        int CustomerId;
-        int? PromotionId; // '?' = Nullable => optional
+        public int PurchaseId { get; set; }
+        public int MovieId;
+        public int ShowingId;
+        public int CustomerId;
+        public int? PromotionId; // '?' = Nullable => optional
 
-        DateTime PurchaseTime;
+        public DateTime PurchaseTime;
         // other purchase info
 
         [Reference]
@@ -93,21 +103,78 @@ namespace CS431_Project.Models
     {
         [AutoIncrement]
         [PrimaryKey]
-        int PromotionId { get; set; }
-        string PromotionCode;
-        DateTime Expiration;
+        public int PromotionId { get; set; }
+        public string PromotionCode;
+        public DateTime Expiration;
 
-        PromotionType PromoType;
+        public PromotionType PromoType;
 
         public enum PromotionType
         {
+            [FriendlyName("Flat rate")]
             FlatRate,
+            [FriendlyName("Percentage")]
             Percentage,
+            [FriendlyName("Free")]
             Free
         }
 
-        decimal PromoValue; // Percentage discount or flat rate discount
+        public decimal PromoValue; // Percentage discount or flat rate discount
     }
 
     //public class Concession
+
+    // http://stackoverflow.com/questions/424366/c-sharp-string-enums
+    public class FriendlyName : System.Attribute
+    {
+        private string _value;
+
+        public FriendlyName(string value)
+        {
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+    }
+
+    public static class StringEnum
+    {
+        public static string GetStringValue(Enum value)
+        {
+            string output = null;
+            Type type = value.GetType();
+
+            //Check first in our cached results...
+
+            //Look for our 'StringValueAttribute' 
+
+            //in the field's custom attributes
+
+            FieldInfo fi = type.GetField(value.ToString());
+            FriendlyName[] attrs = fi.GetCustomAttributes(typeof(FriendlyName), false) as FriendlyName[];
+            if (attrs.Length > 0)
+            {
+                output = attrs[0].Value;
+            }
+
+            return output;
+        }
+
+        public static IEnumerable<SelectListItem> GetSelectListItems<T>()
+        {
+            if (!typeof (T).IsEnum)
+                throw new ArgumentException("GetSelectListItems is only implemented for enums");
+
+            var first = true;
+            foreach (Enum value in Enum.GetValues(typeof(T)))
+            {
+                var val = GetStringValue((Enum)value) ?? value.ToString();
+                yield return new SelectListItem(val, Convert.ChangeType(value, value.GetTypeCode()).ToString(), first);
+                first = false;
+            }
+        }
+    }
 }
