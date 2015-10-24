63766328028c401b68c2a36022ea46442ce30107
George Hahn <george@genericmaker.com>
2015-10-23 22:15:20 -0400

    Add route handler for movie detail view

--- Modules/MovieModule.cs
+++ Modules/MovieModule.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Remoting.Messaging;
 using CS431_Project.Models;
 using Nancy;
 using Nancy.ModelBinding;
@@ -14,6 +15,15 @@ namespace CS431_Project
             {
                 var movies = new MovieController(db);
                 return movies.ListAll();
+            };
+
+            Get["/{movie}"] = req =>
+            {
+                var movies = new MovieController(db);
+                var movie = movies.Lookup(req.movie);
+                if (movie == null)
+                    return 404;
+                return View["MovieDetail", movie];
             };
 
             Get["/create"] = _ => { return View["NewMovie"]; };
